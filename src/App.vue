ff4454d10b85e57914ea79233139701f1bc387b5
Maxime Dénervaud <maxime.denervaud@students.hevs.ch>
2022-10-26 08:16:09 +0200

    function watch

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,22 @@
 <template>
-  <h1 :class="maClasse">Bonjour {{state.user2.name2}}</h1>
-  <input :type="inputType">
-  <h2>Count : {{state.count}}</h2>
-  <button @click="state.count++">add</button>
+  <input type="text" v-model="product.quantity">
   <h3>Prix du produit HT : {{productPriceHT}}</h3>
   <h3>Prix du produit TTC : {{productPriceTTC}}</h3>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, watch } from 'vue';
 import { reactive } from 'vue';
 import { useRoute } from 'vue-router';
 
-
-const name = 'Maxime';  
-const maClasse = 'title';
-const inputType = 'number'; 
-
-const user2 = {
-  name2: 'Tintin',
-  age2: 23
-}
-
-const state = reactive({
-  user2,
-  count:0
-});
-
-setTimeout(() => {
-  state.user2.name2 = 'Maxime';
-},3000)
-
-//typescript
-
-interface User {
-  name: string;
-  age: number | string;
-}
-
-let user: User;
-
-function creatUser(name: string): User{
-  return{
-    name ,
-    age: 22
-  }
-}
-
-user = creatUser('Maxime');
 
 //function computed
 
 const product = reactive ({
   productName: 'book',
   price: 20,
-  quantity: 4
+  quantity: 4,
+  nbChangement: 0
 })  
 
 const productPriceHT = computed(() => { 
@@ -65,11 +27,10 @@ const productPriceTTC = computed(() => {
   return product.price * product.quantity * 1.2
 })
 
+watch(()=> product.quantity,(newValue, oldValue) => {
+  product.nbChangement++;
+  console.log(newValue);
+  console.log(oldValue);
+})
+
 </script>
-
-<style>
-.title{
-  color: red;
-}
-
-</style>
